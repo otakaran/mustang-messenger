0bb06e7d8a037bb40876c45ea9daaa52c1a9e449
Andrew Mariano <corim.corim@gmail.com>
2021-02-10 15:22:16 -0800

    Create React component for signup form

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './App';
-import Header from './header';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Signup from "./Signup";
+import Header from "./header";
+import reportWebVitals from "./reportWebVitals";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
-    <App />
+    <Signup />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
