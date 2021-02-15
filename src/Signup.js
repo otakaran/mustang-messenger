ee259e09dabd71adf8635169b202a9a2f2fd021d
Otakar Andrysek <otakar@me.com>
2021-02-15 10:49:23 -0800

    added firebase configuration to log and sign up

--- src/Signup.js
+++ src/Signup.js
@@ -2,6 +2,26 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "./css/signup.css";
 
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+// Configuration from Firebase admin console
+const firebaseConfig = {
+  apiKey: "AIzaSyAxFXU07_Qkb8z6NbgOUwwD6ImNtURjX1c",
+  authDomain: "mustang-messenger-41f23.firebaseapp.com",
+  projectId: "mustang-messenger-41f23",
+  storageBucket: "mustang-messenger-41f23.appspot.com",
+  messagingSenderId: "195537124874",
+  appId: "1:195537124874:web:3f3a8cb5da2e739390e9f3"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 function Signup() {
   return (
     <div className="signup-component">
