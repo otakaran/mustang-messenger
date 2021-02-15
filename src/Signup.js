d07f7ec652ace9ca85537423a00a1618d635e35b
Otakar Andrysek <otakar@me.com>
2021-02-15 12:14:14 -0800

    sign up mostly functional now

--- src/Signup.js
+++ src/Signup.js
@@ -22,32 +22,64 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+function signUpUserFirebase(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  var email = (data.get('email'));
+  var password = (data.get('password'));
+  var passwordConf = (data.get('passwordConf'));
+  console.log("Doing auth!")
+
+  // TODO REMOVE THIS XD
+  console.log("email: ", email);
+  console.log("password: ", password);
+  console.log("passwordConf: ", passwordConf);
+  //
+
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    console.log("Successfully created user account with uid:", user.uid);
+    alert("Successfully created user account with uid: " + user.uid);
+    // Go to messages now
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log("Error creating user:", errorCode, errorMessage);
+    alert("Error creating user: [" + errorCode + "]\n" + errorMessage);
+    // User can try again
+  });
+}
+
 function Signup() {
   return (
     <div className="signup-component">
       <h2 className="signup-header">Sign up for Mustang Messenger</h2>
-      <Form>
+      <Form onSubmit={signUpUserFirebase}>
         <Form.Group controlId="formUsername">
           <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="Enter username" />
+          <Form.Control type="text" name="username" placeholder="Enter username" />
         </Form.Group>
 
-        <Form.Group controlId="formEmail">
+        <Form.Group controlId="formEmail" id="email">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" name="email" placeholder="Enter email" />
         </Form.Group>
 
-        <Form.Group controlId="formPassword">
+        <Form.Group controlId="formPassword" id="password">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" name="password" placeholder="Password" />
         </Form.Group>
 
         <Form.Group controlId="formPassword2">
           <Form.Label>Repeat password</Form.Label>
-          <Form.Control type="password" placeholder="Repeat password" />
+          <Form.Control type="password" name="passwordConf" placeholder="Repeat password" />
         </Form.Group>
 
-        <Button variant="success" type="submit">
+        <Button variant="success" type="submit" value="Submit">
           Sign up
         </Button>
       </Form>
