bb6b478e77fb9dd1d79b2d3ca3e9eeb4abaf399f
Andrew Mariano <corim.corim@gmail.com>
2021-02-22 13:10:51 -0800

    Fix ESLint warning in header.js

--- src/header.js
+++ src/header.js
@@ -1,37 +1,42 @@
 import { Link } from "react-router-dom";
 import "./css/header.css";
 import banner from "./images/mustang-messenger-banner.png";
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Header extends Component {
-  initialState = {
-    page: 'Welcome!',
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: "Welcome!",
+    };
   }
-
-  state = this.initialState
 
   setCenterText = (text) => {
-      this.setState({
-        ['page']: text,
-      })
-  }
+    this.setState({
+      page: text,
+    });
+  };
 
-  render () {
+  render() {
     return (
-        <div class="header">
+      <div class="header">
         <div class="header-left">
-            <Link to="/" onClick={() => this.setCenterText('Welcome!')}>
+          <Link to="/" onClick={() => this.setCenterText("Welcome!")}>
             <img src={banner} id="banner" alt="Mustang Messenger banner" />
-            </Link>
+          </Link>
         </div>
-        <div class="header-middle">
-            {this.state.page}
-        </div>
+        <div class="header-middle">{this.state.page}</div>
         <div class="header-right">
-            <Link to="/signup" onClick={() => this.setCenterText('Sign Up')}>Sign up</Link> or <Link to="/login" onClick={() => this.setCenterText('Log In')}>Log in</Link>
+          <Link to="/signup" onClick={() => this.setCenterText("Sign Up")}>
+            Sign up
+          </Link>{" "}
+          or{" "}
+          <Link to="/login" onClick={() => this.setCenterText("Log In")}>
+            Log in
+          </Link>
         </div>
-        </div>
-    )
+      </div>
+    );
   }
 }
 
