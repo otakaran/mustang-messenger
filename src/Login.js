41e0120e7e44446a1422c27f3818e3280302349d
Andrew Mariano <corim.corim@gmail.com>
2021-02-22 12:26:33 -0800

    Split login/signup frontend and backend

--- src/Login.js
+++ src/Login.js
@@ -1,38 +1,7 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "./css/signup.css";
-import fire from './Fire.js';
-
-function logInUserFirebase(event) {
-  event.preventDefault();
-  const data = new FormData(event.target);
-  var email = (data.get('email'));
-  var password = (data.get('password'));
-  console.log("Doing login auth!")
-
-  // TODO REMOVE THIS XD
-  console.log("email: ", email);
-  console.log("password: ", password);
-  //
-
-  fire.auth().signInWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    // Signed in
-    var user = userCredential.user;
-    // ...
-    alert("Successfully signed in user account with uid: " + user.uid);
-    // Go to messages now
-    // This isn't the right way of forwarding... 
-    // leaving it for my more React gifted teammates
-    window.location.replace("./MessagePage");
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log("Error logging in:", errorCode, errorMessage);
-    alert("Error logging in: [" + errorCode + "]\n" + errorMessage);
-  });
-}
+import logInUserFirebase from "./loginFire";
 
 function Login() {
   return (
