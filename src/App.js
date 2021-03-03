bafe4a8fc5f10eb9256ab99cb972a3c3af97fe37
Andrew Mariano <corim.corim@gmail.com>
2021-03-03 05:28:37 -0800

    Begin refactoring App and Header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./css/App.css";
-import Header from "./header";
-import Landing from "./landing.js";
+import Header from "./Header";
+import Landing from "./Landing.js";
 import Signup from "./Signup";
 import Login from "./Login";
 import MessagesPage from "./MessagesPage";
 
-class App extends React.Component {
-  render() {
-    return (
+const App = () => {
+  return (
+    <BrowserRouter>
       <main>
         <Header />
-        <Switch>
-          <Route path="/" component={Landing} exact />
-          <Route path="/signup" component={Signup} />
-          <Route path="/login" component={Login} />
-          <Route path="/messages" component={MessagesPage} />
-        </Switch>
+
+        {/* <Switch>
+        <Route path="/" component={Landing} exact />
+        <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login} />
+        <Route path="/messages" component={MessagesPage} />
+      </Switch> */}
       </main>
-    );
-  }
-}
+    </BrowserRouter>
+  );
+};
 
 export default App;
