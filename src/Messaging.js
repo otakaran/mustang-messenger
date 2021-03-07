f034e2a24abbadacafe1aa357288f1cad844951d
ethanchoi1 <ethanchoi19@gmail.com>
2021-03-06 17:26:33 -0800

    Hides 'To: ' field when messaging a contact

--- src/Messaging.js
+++ src/Messaging.js
@@ -1,13 +1,45 @@
+import { useLocation } from "react-router-dom";
 import "./css/messaging.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import sendMessageFirebase from "./fire/sendMessageFire";
 
+// const messageToDefault = () => {
+//   return (
+//     <div>
+//       <label htmlFor="messageTo">To:</label>
+//       <input
+//         type="text"
+//         id="messageTo"
+//         name="messageTo"
+//         placeholder="Enter username here..."
+//       />
+//     </div>
+//   );
+// }
+
+
+
 const Messaging = ({ contact }) => {
-  return (
-    <div class="messaging-component">
-      <Form id="messageForm" onSubmit={sendMessageFirebase}>
-        <label for="messageTo">To:</label>
+  const location = useLocation();
+  console.log("LOCATION.PATHNAME: " + location.pathname)
+
+  const MessageTo = () => {
+    let messageToStyle;
+    console.log("LOCATION.PATHNAME: " + location.pathname)
+    if (location.pathname === "/messages") {
+      messageToStyle = {
+        display: "inline-block",
+      }
+    }
+    else {
+      messageToStyle = {
+        display: "none",
+      }
+    }
+    return(
+      <div style={messageToStyle}>
+        <label htmlFor="messageTo">To:</label>
         <input
           type="text"
           id="messageTo"
@@ -15,6 +47,15 @@ const Messaging = ({ contact }) => {
           placeholder="Enter username here..."
           value={contact}
         />
+      </div>
+    );
+    };
+
+  return (
+    <div className="messaging-component">
+      <Form id="messageForm" onSubmit={sendMessageFirebase}>
+      
+        <MessageTo />
 
         <input
           type="text"
